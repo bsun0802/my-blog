ef71a186cbadb4abbb1a019ca20b2b7e2cdf566c
bsun0802 <bos@usc.edu>
2018-10-21 12:36:29 -0700

    delete commit sh

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# deploy.sh publish the changes to Github pages
-# this script commit.sh will commit ALL changes in source code to origin/master branch.
-
-git add -A
-git commit -m $1
-git push origin master
-
